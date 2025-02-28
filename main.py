1b41b11e5bf4edd7a76f12eb43da36c1380686f7
wolfadlml <rohamrahgozar1388@gmail.com>
2025-02-28 19:07:41 +0330

    Create main.py
    Mosibat

---
+++ main.py
@@ -0,0 +1,42 @@
+import time
+
+print("\033[1;34m< MOSIBAT >\033[0m")
+print("\033[1;31m< MOSIBAT IRANIN BOM  >\033[0m")
+password = "MOSIBAT"
+user_input = input("\033[1;32mPlease enter the password : \033[0m")
+if user_input != password:
+ print("\033[1;31mInvalid password!\033[0m")
+ exit()
+
+ 
+print("\033[1;31m WELCOME USER . .!\033[0m")
+time.sleep(1)
+
+print(" ")
+print(" ")
+print("\033[1;34m ENTER NUMBER . . . .>\033[0m")
+
+print(" ")
+print(" ")
+
+print("\033[1;31m1 - VIRUS Desttructive \033[0m")
+print("\033[1;31m2 - Virus PYTHON \033[0m")
+print("\033[1;31m3 - Virus SiTE \033[0m")
+print("\033[1;31m3 - BACK \033[0m")
+
+choice = input("\033[1;32m Please select an option (1-4): \033[0m")
+print(" ")
+if choice == "1":
+ print("\033[1;34m https://github.com/HACKDRES2/virus-link \033[0m")
+
+ 
+if choice == "2":
+ print("\033[1;34m http://ffmodmenu.page.link/Anti-V \033[0m")
+
+
+if choice == "3":
+ print("\033[1;34m http://Hackweb.xyz/python.html.cs \033[0m")
+
+else:
+ choice == "4"
+ print("BACK ZADEH SHOD . . . . ")
